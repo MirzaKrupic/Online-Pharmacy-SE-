163d73c62c8c8d8bc08ffaa764f8f10d3e8f4193
NaidaFatic <faticnaida@gmail.com>
2022-06-18 19:36:17 +0200

    test

--- api/routes/medicines.php
+++ api/routes/medicines.php
@@ -33,8 +33,7 @@ Flight::route('POST /admin/medicines', function () {
 Flight::route('GET /medicines', function () {
   $offset = Flight::query('offset', 0);
   $limit = Flight::query('limit', 25);
-  if (strlen($search) == 0) $search = " ";
-  else $search = Flight::query('search');
+  $search = " ";
   $order = Flight::query('order', '-id');
 
   $total = Flight::medicineService()->get_medicines($offset, $limit, $search, $order, TRUE);
